1f1db3c090496e008877b01f4abc4f47e794845e
srvivek1 <16172793+SRVivek1@users.noreply.github.com>
2025-01-07 23:35:25 +0530

    gradle hello worl project

---
+++ a1-gradle-basics/src/main/java/com/srvivek/gradle/A1GradleBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.srvivek.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1GradleBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1GradleBasicsApplication.class, args);
+	}
+
+}
---
+++ a1-gradle-basics/src/test/java/com/srvivek/gradle/A1GradleBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srvivek.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1GradleBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
